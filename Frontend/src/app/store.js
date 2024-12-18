60973c26c66d3146f9d77369539cfb447e1337a1
4ustintr <petterpacker2@gmail.com>
2024-12-18 15:30:29 +0700

    first commit

---
+++ Frontend/src/app/store.js
@@ -0,0 +1,15 @@
+import {configureStore} from "@reduxjs/toolkit" 
+import rootRedcuer from "./rootRedcuer";
+import { authApi } from "@/features/api/authApi";
+import { courseApi } from "@/features/api/courseApi";
+import { courseProgressApi } from "@/features/api/courseProgressApi";
+
+export const appStore = configureStore({
+    reducer: rootRedcuer,
+    middleware:(defaultMiddleware) => defaultMiddleware().concat(authApi.middleware, courseApi.middleware, courseProgressApi.middleware)
+});
+
+const initializeApp = async () => {
+    await appStore.dispatch(authApi.endpoints.loadUser.initiate({},{forceRefetch:true}))
+}
+initializeApp();
